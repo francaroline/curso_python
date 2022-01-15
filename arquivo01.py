1a1063f05e58aaedbb55622abe9349f395f70f66
francaroline <94770276+francaroline@users.noreply.github.com>
2022-01-15 11:57:04 -0300

    primeiro

---
+++ arquivo01.py
@@ -0,0 +1,14 @@
+variavel = "aula 01"
+
+var_str_1 = "olá" 
+var_str_2 = " bom dia"
+var_str_3 = (var_str_1 + var_str_2)
+
+print(var_str_3)
+
+var_str_4 = "AGORA FOI"
+var_str_5 = " TUTS TUTS"
+var_str_6 = (var_str_4 + var_str_5)
+
+print(var_str_6)
+
