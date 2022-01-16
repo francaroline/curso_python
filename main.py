cc4cde2df1a9d7e8bfb9083f70bc545a59cf306a
francaroline <94770276+francaroline@users.noreply.github.com>
2022-01-16 17:30:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/status_servidor")
+def status_servidor():
+    return { "online": True }
+
+@app.get("/")
+def root():
+    return {"Online e Roteando!"}
